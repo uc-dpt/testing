2c79e750278a27ae0763ba1a38a131e2dcbbeb51
Deepika Patel <uc-dpt@users.noreply.github.com>
2020-07-18 20:34:36 +0530

    africa testing

--- index.php
+++ index.php
@@ -1,28 +1,42 @@
 <?php
-echo "This is new line";
-echo "This is new line";
-echo "hi";
-//print_r(phpinfo());  This is commented for security reasons.
+// Reads the variables sent via POST from our gateway
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
-function displayData($data)
-{
-	foreach($data as $i)
-	{
-		echo "THIS IS LINE NO:".$i;
-	}
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
+
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+    $response .= "2. Account balance";
+
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
+
+} else if ( $text == "1*2" ) {
+    // This is a second level response where the user selected 1 in the first instance
+    $balance  = "KES 10,000";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your balance is ".$balance;
 }
 
-function displayTable($i){
-	echo "Table of :" . $i . "<br />";;
-	$i = 0;
-	$j = 0;
-	while($j<=10){
-		echo "row => " . $i . "*" . $j . " = " . $i * $j . "<br />";
-	}
-}
-echo "Removed error";
-displayTable(2);
-echo "added";
-
-echo "Hello this is added today by deepika";
-?>
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
